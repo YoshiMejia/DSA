b9fe23468f1924c8dd987eb6b520cd1496a7b6cc
Yoshi <yoshimejia00@gmail.com>
2022-09-17 14:03:16 -0400

    began video notes

---
+++ problem_solving_patterns/sliding_window.js
@@ -0,0 +1,5 @@
+//this pattern involves creating a window which can either be an array or number from one position to another
+//depending on a certain condition, the window either increases or closes (and a new window is created)
+
+// define the maxSubarraySum function which accepts an array of integers and a number called n
+
