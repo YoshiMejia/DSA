b9944a781c2dbc28ee9d9e8fda1c5846bee58955
Yoshi <yoshimejia00@gmail.com>
2022-09-17 14:03:49 -0400

    added code for runningSum youtube video

---
+++ YT/runningSum.js
@@ -0,0 +1,10 @@
+const runningSum = nums => {
+    let result = []
+    let currentSum = 0;
+    for(let i = 0; i < nums.length; i++){
+        let currentValue = nums[i]
+        currentSum += currentValue;
+        result.push(currentSum)
+    }
+    return result;
+} 
