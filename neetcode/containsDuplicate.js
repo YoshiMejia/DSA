3c329dd2c5fe85fd0c117acf5164f2630954ab92
Yoshi <yoshimejia00@gmail.com>
2022-09-18 18:23:16 -0400

    added notes

--- neetcode/containsDuplicate.js
+++ neetcode/containsDuplicate.js
@@ -3,3 +3,7 @@ const containsDuplicate = (nums) => {
     return new Set(nums).size !== nums.length;
 };
 
+let nums = [1,2,2,2,3,4,5]
+new Set(nums) // #=> Set(5) {1, 2, 3, 4, 5}
+//therefore the .size !== nums.length; boolean statement would return true/false 
+
