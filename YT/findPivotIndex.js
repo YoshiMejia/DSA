ff05698d6ffb6bec36e0b17dba533a5e2018d45e
Yoshi <yoshimejia00@gmail.com>
2022-09-19 15:14:46 -0400

    leetcode # 724

---
+++ YT/findPivotIndex.js
@@ -0,0 +1,14 @@
+const findPivotIndex = (nums) => {
+    let totalSum = 0;
+    let leftSum = 0;
+    nums.forEach((num) =>{
+        totalSum += num
+    })
+    for(let i = 0; i < nums.length; i++){
+        if(totalSum - leftSum - nums[i] === leftSum){
+            return i
+        }
+        leftSum += nums[i]
+    }
+    return -1;
+}
