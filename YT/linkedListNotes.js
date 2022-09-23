f30a2ada75f5bad11528156ccecf50bddd408742
Yoshi <yoshimejia00@gmail.com>
2022-09-23 19:17:55 -0400

    first commit -- notes on linkedlist

---
+++ YT/linkedListNotes.js
@@ -0,0 +1,3 @@
+// A linked list is a sequential collection of elements. 
+// each element is called a node and each node has 2 pieces of info-- the data value itself and a pointer that references the next node in the list. the linked list has a head and a tail.
+// the head node points to the second node which points to the third and so on until we reach the tail which points to null
