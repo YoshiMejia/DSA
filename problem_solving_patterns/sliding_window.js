f3a5de393e1e92805093093ff57aebf113332c48
Yoshi <yoshimejia00@gmail.com>
2022-09-28 14:58:42 -0400

    added solution code

--- problem_solving_patterns/sliding_window.js
+++ problem_solving_patterns/sliding_window.js
@@ -3,3 +3,19 @@
 
 // define the maxSubarraySum function which accepts an array of integers and a number called n
 
+function maxSubarraySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+    for(let i = 0; i < num; i++){
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for(let i = num; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+console.log(maxSubarraySum([1,4,5,6,8,5,3,5,5,7,9], 3))
