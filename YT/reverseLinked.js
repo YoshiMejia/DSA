2429997770ee1ac77179e4be6f51a71dfb1a09fc
Yoshi <yoshimejia00@gmail.com>
2022-09-27 21:32:36 -0400

    added console.log of while loop

--- YT/reverseLinked.js
+++ YT/reverseLinked.js
@@ -14,3 +14,25 @@ const reverseLinked = (head) => {
     }
     return prev;
 }
+
+// console.log'ing of the above while loop 
+// next = head.next [2,3,4,5]
+// head.next = prev null
+// prev = head [1]
+// head = next [2,3,4,5]
+// next = head.next [3,4,5]
+// head.next = prev [1]
+// prev = head [2,1]
+// head = next [3,4,5]
+// next = head.next [4,5]
+// head.next = prev [2,1]
+// prev = head [3,2,1]
+// head = next [4,5]
+// next = head.next [5]
+// head.next = prev [3,2,1]
+// prev = head [4,3,2,1]
+// head = next [5]
+// next = head.next null
+// head.next = prev [4,3,2,1]
+// prev = head [5,4,3,2,1]
+// head = next null
