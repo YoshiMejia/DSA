3f91cd4fb3e5672424fbd67df31e6b408c04a531
Yoshi <yoshimejia00@gmail.com>
2022-09-29 12:53:34 -0400

    added comments

--- recursion/recFunc.js
+++ recursion/recFunc.js
@@ -1,10 +1,14 @@
 const recFunc = (num) => {
     if(num <= 0){
         console.log('All done!')
+        // return statement here is for breaking out of the loop, it doesn't return anything
         return;
     }
+    // log the curr value of num
     console.log(num)
+    // decrease it for the next function call
     num--;
+    // call this function again until the conditional above is true
     recFunc(num)
 }
 
