23ce7de84261a72d7ae0d880fc941c3aa71f1706
Yoshi <yoshimejia00@gmail.com>
2022-09-30 16:30:37 -0400

    for visibility, added version with/without comments

--- YT/isomorphicStrings.js
+++ YT/isomorphicStrings.js
@@ -1,21 +1,34 @@
 
+// const isIsomorphic = function(s, t) {
+//     //define 2 empty objects to hold the letters and index counts
+//     const objS = {},
+//         objT = {}
+//     for(let i = 0; i < s.length; i++){
+//         // if the character found at s[i] is NOT inside of objS, and the character at t[i] is NOT in objT
+//         if(!(s[i] in objS) && !(t[i] in objT)){
+//             //set the count at that key in the object to the index of the other string at the current index
+//             objS[s[i]] = t[i],
+//             objT[t[i]] = s[i]
+//         } else {
+//             //else, if those characters ARE found in the objects
+//             // if the value associated to the key in this current index for string S is NOT EQUAL to the character at string T, return false
+//             if(objS[s[i]] !== t[i]) return false
+//         }
+//     }
+//     // return true if all other tests have passed
+//     return true
+// };
 const isIsomorphic = function(s, t) {
-    //define 2 empty objects to hold the letters and index counts
     const objS = {},
         objT = {}
     for(let i = 0; i < s.length; i++){
-        // if the character found at s[i] is NOT inside of objS, and the character at t[i] is NOT in objT
         if(!(s[i] in objS) && !(t[i] in objT)){
-            //set the count at that key in the object to the index of the other string at the current index
             objS[s[i]] = t[i],
             objT[t[i]] = s[i]
         } else {
-            //else, if those characters ARE found in the objects
-            // if the value associated to the key in this current index for string S is NOT EQUAL to the character at string T, return false
-            if(objS[s[i]] !== t[i]) return false
+              if(objS[s[i]] !== t[i]) return false
         }
     }
-    // return true if all other tests have passed
     return true
 };
 
